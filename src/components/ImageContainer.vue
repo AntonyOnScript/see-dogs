2fc092f529f42b132f52035d7dd88aee1badea69
AntonyOnScript <santhiagoafk@gmail.com>
2021-11-11 13:37:06 -0300

    Random dogs show has been prototyped

---
+++ src/components/ImageContainer.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-for="image of images" :key="image">
+          <img :src="image" alt="">
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        images: {
+            required: true,
+            type: Array
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,36 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <h1>{{ title }}</h1>
+    <hr>
+    <div class="dogs-container">
+      <image-container :images="images"></image-container>
+    </div>
+  </div>
 </template>
 
 <script>
+import ImageContainer from "../components/ImageContainer.vue"
+
 export default {
+  components: {
+    "image-container": ImageContainer
+  },
+  data() {
+    return {
+      title: "Home",
+      images: []
+    }
+  },
+  methods: {
+    randomDogs(quantity) {
+      this.$http.get("https://dog.ceo/api/breeds/image/random/"+quantity)
+      .then(response => {
+        this.images = response.body.message
+      })
+    }
+  },
+  created() {
+    this.randomDogs(10)
+  }
 }
 </script>
