6aefc50e3a5c3f67ec2369dcfa97d86778d39d12
AntonyOnScript <santhiagoafk@gmail.com>
2021-11-10 21:22:50 -0300

    Vue-cli trash have been removed

---
+++ src/router/routes.js
@@ -0,0 +1,15 @@
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+export const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    }
+]
