141b3d8d2a923f6435a07c8162b31db17eda3632
AntonyOnScript <santhiagoafk@gmail.com>
2022-04-28 22:16:06 -0300

    fix: 404 on aws

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,7 @@ import { routes } from './routes'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
+    base: process.env.BASE_URL  || '/',
     routes
 })
 
